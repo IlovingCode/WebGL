9341ae154592d44d989d29ec6d54f6071feaa2a3
thien.nguyencong <thien.nguyencong@gameloft.com>
2019-03-28 19:27:46 +0700

    - add scale effect

--- webGLSample/data/app.js
+++ webGLSample/data/app.js
@@ -15,23 +15,37 @@ var fragmentShaderText = `
 	precision mediump float;
     varying vec2 vUv;
 	uniform sampler2D texture;
+	uniform sampler2D disp;
 	uniform float time;
+	uniform float ratio;
+	uniform vec2 mouse;
+
+	float PI = 3.141592;
 
 	float rand(vec2 co){
 		return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 	}
 
     void main() {
-		vec2 uv = vUv;
-		uv.x += (uv.x - 0.5) * time / pow(1.0 - uv.y, 2.0);
+		vec2 uv = vUv; 
+		vec2 n = normalize(uv - mouse);
+		float length = length(uv - mouse);
+		float a = PI * time * 0.5;
+		vec2 n2 = vec2(0.0);
+		n2.x = n.x * cos(a) - n.y * sin(a);
+		n2.y = n.x * sin(a) + n.y * cos(a);
+		vec4 dmap = texture2D(disp, n2);
+		uv += n2 * time * time  + dmap.r * length * 0.1;
 		vec4 c= texture2D(texture, uv);
-		c *= step(0.0, uv.x) * step(uv.x, 1.0);
+		c *= step(0.0, uv.x) * step(uv.x, 1.0) * step(0.0, uv.y) * step(uv.y, 1.0);
+		dmap *= step(0.0, uv.x) * step(uv.x, 1.0) * step(0.0, uv.y) * step(uv.y, 1.0);
+		c = mix(dmap, c, 0.8);
         gl_FragColor = c;
     }`;
 
 var param = 0;
 var updateAttribute = function (dt) {
-	param += dt;
+	param += dt * 0.5;
 	var loc1 = gl.getUniformLocation(shaderProg, 'time');
 	gl.uniform1f(loc1, param - Math.floor(param));
 }
@@ -194,7 +208,7 @@ var handleStart = function (evt) {
 }
 
 var handleMove = function (evt) {
-	console.log('touchmove');
+	//console.log('touchmove');
 	var loc2 = gl.getUniformLocation(shaderProg, 'mouse');
 	gl.uniform2f(loc2, evt.x / canvas.width, evt.y / canvas.height);
 
