8325bc5ca4d63a7c670d4ef6151d072af5a61b84
IlovingCode <ncthien93@gmail.com>
2019-06-15 22:19:08 +0700

    - update

--- webGLSample/data/colors.js
+++ webGLSample/data/colors.js
@@ -21,39 +21,49 @@ colors = {
 	uniform vec2 mouse;
 
 	float s = 0.392156;
-	vec3 c1 = vec3(1.0, s, 1.0);
-	vec3 c2 = vec3(s, 1.0, 1.0);
-	vec3 c3 = vec3(1.0, 1.0, s);
-	vec3 c4 = vec3(s, s, 1.0);
-	vec3 c5 = vec3(1.0, s, s);
-	vec3 c6 = vec3(s, 1.0, s);
+	vec4 c1 = vec4(1.0, s, 1.0, 1.0);
+	vec4 c2 = vec4(s, 1.0, 1.0, 1.0);
+	vec4 c3 = vec4(1.0, 1.0, s, 1.0);
+	vec4 c4 = vec4(s, s, 1.0, 1.0);
+	vec4 c5 = vec4(1.0, s, s, 1.0);
+	vec4 c6 = vec4(s, 1.0, s, 1.0);
+
+	vec4 check(float t, float a, vec4 c) {
+		return step(t, a) * step(a, t + 1.0) * c;
+	}
 
     void main() {
 		float ratio = resolution.x / resolution.y; 
 		vec2 d = vUv - mouse;
+		float l = length(d) * 3.0;
 		float ax = d.x * d.x / 0.04 + d.y * d.y / ratio / ratio / 0.04;
 		ax /= radius;
-		float a = atan(d.x * ratio, d.y) / 3.14159265359 + 1.0;
-		a *= floor(time);
-		a = floor(a);
-		vec4 c = vec4(0.0, 0.0, 0.0, 1.0);
-		vec3 t = //vec3(a, a, a)
-				+step(a, 1.0) * c1 
-				+step(1.01, a) * step(a, 3.0) * c2
-				+step(3.01, a) * step(a, 5.0) * c3
-				+step(5.01, a) * step(a, 7.0) * c4
-				+step(7.01, a) * step(a, 9.0) * c5
-				+step(9.01, a) * c6
-				;
-		t *= step(1.0, ax) * 2.0 + 1.0;
-		c = step(ax, 1.1) * vec4(t, c.a) + step(1.1, ax) * c;
+		float a = 1.0 + atan(d.x * ratio, d.y) * 0.318309886; // 1/Pi
+		a *= time * 0.5;
+        // atan: [-Pi, Pi] -> a: [0, 2.0 * a1f]
+		vec4 c = 
+			check(0.0 + l, a, c1)
+          + check(1.0 + l, a, c2)
+          + check(2.0 + l, a, c3)
+          + check(3.0 + l, a, c4)
+          + check(4.0 + l, a, c5)
+		  + check(5.0 + l, a, c6)
+		  + check(0.0 + l, time, c1) * step(a, l)
+		  + check(1.0 + l, time, c2) * step(a, l)
+		  + check(2.0 + l, time, c3) * step(a, l)
+		  + check(3.0 + l, time, c4) * step(a, l)
+		  + check(4.0 + l, time, c5) * step(a, l)
+		  + check(5.0 + l, time, c6) * step(a, l)
+        ;
+        c.rgb *= step(0.1, fract(a - l)) * step(ax, 0.9);
+		c = step(ax, 1.0) * c + step(1.0, ax) * vec4(0);
         gl_FragColor = c;
     }`,
 
 	updateAttribute: function (dt) {
-		param += dt * 3;
+		param += dt;
 		var loc1 = gl.getUniformLocation(shaderProg, 'time');
-		gl.uniform1f(loc1, Math.abs(param % 14 - 7));
+		gl.uniform1f(loc1, Math.floor(1 + Math.abs(param % 12 - 6)));
 	},
 
 	texList: [],
