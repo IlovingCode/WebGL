accb225e2ba9bc99ada35f328df26e836c598dac
thien.nguyencong <thien.nguyencong@gameloft.com>
2019-06-25 12:35:51 +0700

    - update water

--- webGLSample/data/water.js
+++ webGLSample/data/water.js
@@ -13,7 +13,7 @@ water = {
     varying vec2 vUv;
     uniform sampler2D texture;
     uniform float time;
-	float radius = .07;
+	float radius = .05;
 	uniform vec2 resolution;
 	uniform vec2 mouse;
 
@@ -23,19 +23,18 @@ water = {
 		float ax = d.x * d.x + d.y * d.y;
         //ax /= radius;
         
-        vec2 uv = vec2(vUv.x, vUv.y);
         float t = time;
         float a = ax - radius * t;
-        a *= 3.141592 * 10.;
+        a *= 30.;
 
-        float h = 1e-2;
+        float h = 1e-1;
         float d1 = a - h;
         float d2 = a + h;
         float p1 = sin(d1) * smoothstep(-.5, -.1, d1) * smoothstep(-.1, -.5, d1);
         float p2 = sin(d2) * smoothstep(-.5, -.1, d2) * smoothstep(-.1, -.5, d2);
         vec2 circles = t * (1. - t) * normalize(d) * (p2 - p1);
 
-        vec3 color = texture2D(texture, uv - circles).rgb;
+        vec3 color = texture2D(texture, vUv - circles).rgb;
         //vec3 colord = vec3(uv - circles, 0.) * 1.;
         gl_FragColor = vec4(color, 1.0);
     }`,
