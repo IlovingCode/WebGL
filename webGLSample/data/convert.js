bca39db0e8ae57d82db421b8ca0d5dc2802ba083
thien.nguyencong <thien.nguyencong@gameloft.com>
2019-07-31 20:12:03 +0700

    - update

--- webGLSample/data/convert.js
+++ webGLSample/data/convert.js
@@ -20,122 +20,39 @@ convert = {
 	uniform vec2 resolution;
 	uniform vec2 mouse;
 
-	lowp float RGBToL(lowp vec3 color)
-    {
-        lowp float fmin = min(min(color.r, color.g), color.b);    //Min. value of RGB
-        lowp float fmax = max(max(color.r, color.g), color.b);    //Max. value of RGB
-        
-        return (fmax + fmin) / 2.0; // Luminance
-    }
- 
-    lowp vec3 RGBToHSL(lowp vec3 color)
-    {
-        lowp vec3 hsl; // init to 0 to avoid warnings ? (and reverse if + remove first part)
-
-        lowp float fmin = min(min(color.r, color.g), color.b);    //Min. value of RGB
-        lowp float fmax = max(max(color.r, color.g), color.b);    //Max. value of RGB
-        lowp float delta = fmax - fmin;             //Delta RGB value
-
-        hsl.z = (fmax + fmin) / 2.0; // Luminance
-
-        if (delta == 0.0)		//This is a gray, no chroma...
-        {
-            hsl.x = 0.0;	// Hue
-            hsl.y = 0.0;	// Saturation
-        }
-        else                                    //Chromatic data...
-        {
-            if (hsl.z < 0.5)
-                hsl.y = delta / (fmax + fmin); // Saturation
-            else
-                hsl.y = delta / (2.0 - fmax - fmin); // Saturation
-            
-            lowp float deltaR = (((fmax - color.r) / 6.0) + (delta / 2.0)) / delta;
-            lowp float deltaG = (((fmax - color.g) / 6.0) + (delta / 2.0)) / delta;
-            lowp float deltaB = (((fmax - color.b) / 6.0) + (delta / 2.0)) / delta;
-            
-            if (color.r == fmax )
-                hsl.x = deltaB - deltaG; // Hue
-            else if (color.g == fmax)
-                hsl.x = (1.0 / 3.0) + deltaR - deltaB; // Hue
-            else if (color.b == fmax)
-                hsl.x = (2.0 / 3.0) + deltaG - deltaR; // Hue
-            
-            if (hsl.x < 0.0)
-                hsl.x += 1.0; // Hue
-            else if (hsl.x > 1.0)
-                hsl.x -= 1.0; // Hue
-        }
-
-        return hsl;
-    }
-
-    lowp float HueToRGB(lowp float f1, lowp float f2, lowp float hue)
-    {
-        if (hue < 0.0)
-            hue += 1.0;
-        else if (hue > 1.0)
-            hue -= 1.0;
-        lowp float res;
-        if ((6.0 * hue) < 1.0)
-            res = f1 + (f2 - f1) * 6.0 * hue;
-        else if ((2.0 * hue) < 1.0)
-            res = f2;
-        else if ((3.0 * hue) < 2.0)
-            res = f1 + (f2 - f1) * ((2.0 / 3.0) - hue) * 6.0;
-        else
-            res = f1;
-        return res;
-    }
-
-    lowp vec3 HSLToRGB(lowp vec3 hsl)
-    {
-        lowp vec3 rgb;
-
-        if (hsl.y == 0.0)
-            rgb = vec3(hsl.z); // Luminance
-        else
-        {
-            lowp float f2;
-            
-            if (hsl.z < 0.5)
-                f2 = hsl.z * (1.0 + hsl.y);
-            else
-                f2 = (hsl.z + hsl.y) - (hsl.y * hsl.z);
-            
-            lowp float f1 = 2.0 * hsl.z - f2;
-            
-            rgb.r = HueToRGB(f1, f2, hsl.x + (1.0/3.0));
-            rgb.g = HueToRGB(f1, f2, hsl.x);
-            rgb.b= HueToRGB(f1, f2, hsl.x - (1.0/3.0));
-        }
-
-        return rgb;
+	vec3 rgb2hsv (vec3 c) {
+		vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+		vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+		vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+	
+		float d = q.x - min(q.w, q.y);
+		float e = 1.0e-10;
+		return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
 	}
 	
-	void main()
-    {
-        lowp vec4 textureColor = texture2D(texture, vUv);
-        
-        lowp float lightness = RGBToL(textureColor.rgb);
-        lowp vec3 shift = vec3(1., 1., 0.);
-
-        const lowp float a = 0.25;
-        const lowp float b = 0.333;
-        const lowp float scale = 0.7;
-
-        lowp vec3 midtones = (clamp((lightness - b) /  a + 0.5, 0.0, 1.0) * clamp ((lightness + b - 1.0) / -a + 0.5, 0.0, 1.0) * scale) * shift;
-
-        lowp vec3 newColor = textureColor.rgb + midtones;
-        newColor = clamp(newColor, 0.0, 1.0);
-
-        // preserve luminosity
-        lowp vec3 newHSL = RGBToHSL(newColor);
-        lowp float oldLum = RGBToL(textureColor.rgb);
-        textureColor.rgb = HSLToRGB(vec3(newHSL.x, newHSL.y, oldLum));
-        
-        gl_FragColor = textureColor;
-    }`,
+	vec3 hsv2rgb (vec3 c) {
+		vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+		vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+		return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+	}
+	
+	void main() {
+		vec4 px = texture2D(texture, vUv);
+		vec4 mx = texture2D(texture, mouse);
+	
+		float r = 0.05;
+		float t = 0.2;
+			
+		vec3 hsv = rgb2hsv (px.rgb);
+		vec3 tar = rgb2hsv (mx.rgb);
+		vec3 xcg = rgb2hsv (vec3 (1.0, 0.0, 0.0));
+		
+		float d = distance (hsv.x, tar.x);
+		hsv.x = xcg.x;
+		
+			
+		gl_FragColor = mix (vec4 (hsv2rgb (hsv), 1.0), px, smoothstep (t-r, t+r, d));	
+	}`,
 
 	texList: [
 		{ path: 'dmap.jpg', location: 'disp' },
